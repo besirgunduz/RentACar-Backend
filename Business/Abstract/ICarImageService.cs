d6d8281ca7c10fc78d0bbf43f9532b069961d6a2
Besir Gunduz <besirgunduz1993@gmail.com>
2021-05-23 22:57:34 +0300

    23.05.2021 Commit 1

---
+++ Business/Abstract/ICarImageService.cs
@@ -0,0 +1,14 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICarImageService
+    {
+        IDataResult<List<CarImage>> GetAll();
+        IResult Add(CarImage entity);
+    }
+}
---
+++ WebAPI/Controllers/CarImagesController.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarImagesController : ControllerBase
+    {
+        ICarImageService _carImageService;
+        public CarImagesController(ICarImageService carImageService)
+        {
+            _carImageService = carImageService;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _carImageService.GetAll();
+            return Ok(result);
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add(CarImage carImage)
+        {
+           var result = _carImageService.Add(carImage);
+            return Ok(result);
+        }
+
+    }
+}
