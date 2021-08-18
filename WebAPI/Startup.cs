50442ecf7fbb78cb41edd488dd83af87b5f7914b
BESIR GUNDUZ <besirgunduz1993@gmail.com>
2021-08-18 19:45:56 +0300

    added swagger

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace WebAPI
 {
@@ -19,6 +20,10 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +44,9 @@ namespace WebAPI
             {
                 endpoints.MapControllers();
             });
+            //swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "denemeApi v1"));
         }
     }
 }
